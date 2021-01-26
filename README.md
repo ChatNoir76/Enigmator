8e936f9e64211bd1dc70174a37a4cf10a263f992
ChatNoir76 <chatnoirvip@gmail.com>
2021-01-26 18:26:43 +0100

    Eclipse RCP 3 Based Application

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Enigmator
 Application de cryptage de texte
+
+<br>Basé sur une architecture Eclipse RCP 3
+<br>Target platform 2020/12
+<br>Java 1.8
+<br>projet initialisé sur la branch main, développement en cours sur la branche develop
+
---
+++ fr.chatnoir.enigmator/src/fr/chatnoir/enigmator/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package fr.chatnoir.enigmator;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "fr.chatnoir.enigmator.perspective";
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
