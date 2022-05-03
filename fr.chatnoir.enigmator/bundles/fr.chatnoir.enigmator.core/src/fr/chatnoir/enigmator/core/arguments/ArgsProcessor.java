fd19827118de1119fdef824c9545a7b9c449bfc7
ChatNoir76 <chatnoirvip@gmail.com>
2022-05-03 23:18:27 +0200

    simplified args system

--- fr.chatnoir.enigmator/bundles/fr.chatnoir.enigmator.core/src/fr/chatnoir/enigmator/core/arguments/ArgsProcessor.java
+++ fr.chatnoir.enigmator/bundles/fr.chatnoir.enigmator.core/src/fr/chatnoir/enigmator/core/arguments/ArgsProcessor.java
@@ -1,11 +1,8 @@
 package fr.chatnoir.enigmator.core.arguments;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 /**
  * Classe de création de processus en fonction des arguments de l'application
@@ -13,75 +10,51 @@ import java.util.StringTokenizer;
  *
  */
 public class ArgsProcessor {
+	
+	private final static String[] ARG_DECRYPT = {"-d:", "--decrypt:"};
+	private final static String[] ARG_DECRYPT_FILE = {"-fd:", "--filedecrypt:"};
+	private final static String[] ARG_ENCRYPT = {"-e:", "--encrypt:"};
+	private final static String[] ARG_ENCRYPT_FILE = {"-fe:", "--fileencrypt:"};
+	private final static String[] ARG_HELP = {"-h", "--help"};
 
-	private final static char ARG_SEPARATOR = ':';
-	private final static char ARG_START = '-';
+	private Map<ArgsType, String[]> liste;
+	private ArgsType type;
+	private String arg;
 	
-	private final static String[] ARG_DECRYPT = {"d", "decrypt"};
-	private final static String[] ARG_DECRYPT_FILE = {"fd", "filedecrypt"};
-	private final static String[] ARG_ENCRYPT = {"e", "encrypt"};
-	private final static String[] ARG_ENCRYPT_FILE = {"fe", "fileencrypt"};
-	private final static String[] ARG_HELP = {"h", "help"};
-	
-	private ArrayList<String> decrypt = new ArrayList<String>();
-	private ArrayList<String> encrypt = new ArrayList<String>();
-	private ArrayList<String> filedecrypt = new ArrayList<String>();
-	private ArrayList<String> fileencrypt = new ArrayList<String>();
-	private ArrayList<String> help = new ArrayList<String>();
+	public ArgsType getType() {
+		return type;
+	}
 
-	private Map<ArgsType, String> liste;
-	
-	public Map<ArgsType, String> getListe() {
-		return liste;
+	public String getArg() {
+		return arg;
 	}
 	
-	private void initialize() {
-		decrypt.addAll(Arrays.asList(ARG_DECRYPT));
-		encrypt.addAll(Arrays.asList(ARG_ENCRYPT));
-		help.addAll(Arrays.asList(ARG_HELP));
-		filedecrypt.addAll(Arrays.asList(ARG_DECRYPT_FILE));
-		fileencrypt.addAll(Arrays.asList(ARG_ENCRYPT_FILE));
+	private void init() {
+		this.type = ArgsType.None;
+		this.arg = null;
+		this.liste = new HashMap<ArgsType, String[]>();
+		liste.put(ArgsType.Decrypt, ARG_DECRYPT);
+		liste.put(ArgsType.DecryptFromFile, ARG_DECRYPT_FILE);
+		liste.put(ArgsType.Encrypt, ARG_ENCRYPT);
+		liste.put(ArgsType.EncryptFromFile, ARG_ENCRYPT_FILE);
+		liste.put(ArgsType.HelpInformation, ARG_HELP);
 	}
 
 	public ArgsProcessor(List<String> args) {
-		liste = new HashMap<ArgsType, String>();
-		this.initialize();
+		this.init();
 		
-		StringBuilder Delimiter = new StringBuilder();
-		Delimiter.append(ARG_SEPARATOR);
-		Delimiter.append(ARG_START);
-				
 		if(!args.isEmpty()) {
-			args.forEach(argument -> {
-				//traitement des arguments
-				StringTokenizer strTok = new StringTokenizer(argument, Delimiter.toString());
-				int iterator = strTok.countTokens();
-				//initialisation des variables
-				ArgsType type = ArgsType.None;
-				String key = null;
-				String value = null;
-				
-				if(iterator == 2) {
-					key = strTok.nextToken();
-					value = strTok.nextToken();
-					
-					if(decrypt.contains(key)) {
-						type = ArgsType.Decrypt;
-					} else if(encrypt.contains(key)) {
-						type = ArgsType.Encrypt;
-					} else if(filedecrypt.contains(key)) {
-						type = ArgsType.DecryptFromFile;
-					} else if(fileencrypt.contains(key)) {
-						type = ArgsType.EncryptFromFile;
-					}
-												
-				} else if(iterator == 1) {
-					key = strTok.nextToken();
-					if(help.contains(key)) {
-						type = ArgsType.HelpInformation;
+			StringBuilder arg = new StringBuilder();
+			args.forEach(s -> arg.append(s).append(" "));
+			arg.deleteCharAt(arg.length()-1);
+			
+			liste.forEach((key, values) -> {
+				for(String value: values) {
+					if(arg.toString().startsWith(value)) {
+						this.type = key;
+						this.arg  = arg.substring(value.length());
 					}
 				}
-				liste.put(type, value);
 			});
 		} 
 	}
